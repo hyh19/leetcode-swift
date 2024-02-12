0ecc8d7b5e7d7333a8bfeb79b73f0cc1641fe49b
spadkins <spadkins@outlook.com>
2024-02-12 23:52:49 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,33 +12,14 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 将数组中的元素向右旋转 k 个位置。
-    /// - Parameters:
-    ///   - nums: 一个整型数组的引用，操作将直接在这个数组上进行。
-    ///   - k: 旋转的步数，即将数组的最后 k 个元素移动到数组的前面。
-    /// 数组旋转操作首先对整个数组进行反转，然后分别反转前 k 个元素和剩余的 n-k 个元素。
-    func rotate(_ nums: inout [Int], _ k: Int) {
-        let n = nums.count // 数组的长度
-        let t = k % n // 实际旋转的步数，考虑到 k 可能大于数组长度的情况
-        if t > 0 {
-            reverse(&nums, 0, n - 1) // 先反转整个数组
-            reverse(&nums, 0, t - 1) // 然后反转前 t 个元素
-            reverse(&nums, t, n - 1) // 最后反转剩余的元素
-        }
-    }
-
-    /// 将数组的一部分进行反转。
-    /// - Parameters:
-    ///   - nums: 一个整型数组的引用，操作将直接在这个数组上进行。
-    ///   - lo: 要反转部分的起始索引。
-    ///   - hi: 要反转部分的结束索引。
-    /// 此方法通过交换两端的元素来反转指定部分的数组，直到中间位置。
-    private func reverse(_ nums: inout [Int], _ lo: Int, _ hi: Int) {
-        var i = lo, j = hi
-        while i < j {
-            nums.swapAt(i, j) // 交换 i 和 j 位置的元素
-            i += 1 // i 向中间移动
-            j -= 1 // j 向中间移动
-        }
+    /// 将整数数组转换为能够组成的最大数的字符串表示
+    ///
+    /// - Parameter nums: 一个整数数组
+    /// - Returns: 一个表示数组中数字能组成的最大数的字符串
+    func largestNumber(_ nums: [Int]) -> String {
+        var strs = nums.map { String($0) } // 将整数数组转换为字符串数组
+        strs.sort(by: { $0 + $1 > $1 + $0 }) // 根据组合后的字符串大小进行排序
+        let ans = strs.joined() // 将排序后的字符串数组拼接成一个单一的字符串
+        return ans[ans.startIndex] == "0" ? "0" : ans // 检查结果字符串的开头是否为 “0”
     }
 }
