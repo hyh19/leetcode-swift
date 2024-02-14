0ffe2a33d4034dacc27ebcdb131212c8abf5d331
spadkins <spadkins@outlook.com>
2024-02-14 15:35:28 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,13 +12,25 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 在一个升序排列的整型数组中执行二分查找
+    /// 查找排序数组中指定元素的首尾位置
     ///
     /// - Parameters:
-    ///   - nums: 一个升序排列的整型数组
+    ///   - nums: 排序好的整数数组
     ///   - target: 需要查找的目标值
-    /// - Returns: 如果目标值存在于数组中，则返回其索引；否则返回 -1。
-    func search(_ nums: [Int], _ target: Int) -> Int {
+    /// - Returns: 返回一个包含目标值首次出现和最后一次出现的索引位置的数组。如果目标值在数组中不存在，则返回 [-1, -1]。
+    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+        return [binarySearch(nums, target, true), binarySearch(nums, target, false)]
+    }
+
+    /// 在排序数组中执行二分查找，以找到目标值的索引位置。
+    ///
+    /// - Parameters:
+    ///   - nums: 排序好的整数数组
+    ///   - target: 需要查找的目标值
+    ///   - lower: 布尔值，指示查找目标值的首次出现（true）还是最后一次出现（false）。
+    /// - Returns: 如果找到目标值，则返回其索引位置；否则返回 -1。
+    private func binarySearch(_ nums: [Int], _ target: Int, _ lower: Bool) -> Int {
+        var res = -1
         var lo = 0
         var hi = nums.count - 1
         while lo <= hi {
@@ -28,9 +40,14 @@ class Solution {
             } else if nums[mid] < target {
                 lo = mid + 1
             } else {
-                return mid
+                res = mid
+                if lower {
+                    hi = mid - 1
+                } else {
+                    lo = mid + 1
+                }
             }
         }
-        return -1
+        return res
     }
 }
