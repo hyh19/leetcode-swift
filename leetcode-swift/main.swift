d9884ccea9fbd40b798e078bd856cc292477ab01
spadkins <spadkins@outlook.com>
2024-02-15 16:20:39 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,42 +12,36 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 查找排序数组中指定元素的首尾位置
+    /// 在旋转排序数组中搜索一个给定的目标值
     ///
     /// - Parameters:
-    ///   - nums: 排序好的整数数组
-    ///   - target: 需要查找的目标值
-    /// - Returns: 返回一个包含目标值首次出现和最后一次出现的索引位置的数组。如果目标值在数组中不存在，则返回 [-1, -1]。
-    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-        return [binarySearch(nums, target, true), binarySearch(nums, target, false)]
-    }
-
-    /// 在排序数组中执行二分查找，以找到目标值的索引位置。
-    ///
-    /// - Parameters:
-    ///   - nums: 排序好的整数数组
-    ///   - target: 需要查找的目标值
-    ///   - lower: 布尔值，指示查找目标值的首次出现（true）还是最后一次出现（false）。
-    /// - Returns: 如果找到目标值，则返回其索引位置；否则返回 -1。
-    private func binarySearch(_ nums: [Int], _ target: Int, _ lower: Bool) -> Int {
-        var res = -1
+    ///   - nums: 旋转排序的数组
+    ///   - target: 要搜索的目标值
+    /// - Returns: 目标值在数组中的索引；如果不存在，则返回 `-1`。
+    func search(_ nums: [Int], _ target: Int) -> Int {
         var lo = 0
         var hi = nums.count - 1
         while lo <= hi {
             let mid = lo + (hi - lo) / 2
-            if target < nums[mid] {
-                hi = mid - 1
-            } else if nums[mid] < target {
-                lo = mid + 1
+            if nums[mid] == target {
+                return mid
+            }
+            // 当 lo = hi 时，nums[lo] = nums[mid] = nums[hi]，
+            // 此时 nums[mid] < target <= nums[hi] 不成立
+            if nums[mid] <= nums[hi] {
+                if nums[mid] < target && target <= nums[hi] {
+                    lo = mid + 1
+                } else {
+                    hi = mid - 1
+                }
             } else {
-                res = mid
-                if lower {
+                if nums[lo] <= target && target < nums[mid] {
                     hi = mid - 1
                 } else {
                     lo = mid + 1
                 }
             }
         }
-        return res
+        return -1
     }
 }
