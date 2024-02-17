657dad2c7eb913cb5703019fc80a2a2b004e3276
spadkins <spadkins@outlook.com>
2024-02-17 17:09:08 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,22 +12,19 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 计算在指定天数内送达所有包裹需要的最小运载能力
-    ///
-    /// 使用二分查找算法找到满足条件的最小运载能力。定义两个边界，下边界为包裹中最重的重量，上边界为所有包裹重量之和。
-    /// 逐步缩小这个范围直到找到最小的符合条件的运载能力。
+    /// 计算 Koko 吃完所有香蕉的最小速度
     ///
     /// - Parameters:
-    ///   - weights: 包裹重量的数组
-    ///   - days: 需要在这么多天内送达所有包裹
-    /// - Returns: 返回能在指定天数内完成运送的最小运载能力
-    func shipWithinDays(_ weights: [Int], _ days: Int) -> Int {
-        var lo = weights.max()! // 所有包裹中的最大重量，为运载能力的最小可能值
-        var hi = weights.reduce(0, +) // 所有包裹的总重量，为运载能力的最大可能值
+    ///   - piles: 一个整数数组，每个元素代表一堆香蕉的数量。
+    ///   - h: 一个整数，表示总时间限制，单位为小时。
+    /// - Returns: 返回 Koko 吃完所有香蕉的最小速度
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var lo = 1
+        var hi = piles.max()!
         var ans = lo
         while lo <= hi {
             let mid = lo + (hi - lo) / 2
-            if canFinish(weights, days, mid) {
+            if canFinish(piles, h, mid) {
                 ans = mid
                 hi = mid - 1
             } else {
@@ -37,28 +34,18 @@ class Solution {
         return ans
     }
 
-    /// 检查给定运载能力是否能在指定天数内完成包裹运送
-    ///
-    /// 根据给定的运载能力和包裹重量数组，计算出在不超过运载能力的情况下，完成所有包裹运送需要的天数。
-    /// 如果这个天数小于或等于给定的天数，返回 true，表示可以完成运送。
+    /// 判断给定速度 k 下，是否能在 h 小时内吃完所有香蕉。
     ///
     /// - Parameters:
-    ///   - weights: 包裹重量的数组
-    ///   - days: 目标天数
-    ///   - capacity: 给定的运载能力
-    /// - Returns: 如果能在指定天数内完成运送，返回 true；否则返回 false。
-    private func canFinish(_ weights: [Int], _ days: Int, _ capacity: Int) -> Bool {
-        var minDays = 0 // 完成运送需要的最小天数
-        var i = 0 // 当前考察的包裹索引
-        let n = weights.count
-        while i < n {
-            var sum = 0 // 当前船上包裹的总重量
-            while i < n && sum + weights[i] <= capacity { // 当前船的总重量不超过给定的运载能力
-                sum += weights[i]
-                i += 1
-            }
-            minDays += 1 // 完成一天的运送
+    ///   - piles: 一个整数数组，每个元素代表一堆香蕉的数量。
+    ///   - h: 一个整数，表示总时间限制，单位为小时。
+    ///   - k: 一个整数，表示 Koko 每小时吃香蕉的速度。
+    /// - Returns: 如果能在 h 小时内吃完返回 `true`，否则返回 `false`。
+    private func canFinish(_ piles: [Int], _ h: Int, _ k: Int) -> Bool {
+        var hours = 0
+        for p in piles {
+            hours += (p - 1) / k + 1
         }
-        return minDays <= days // 如果需要的天数不超过给定的天数，返回 true
+        return hours <= h
     }
 }
