b591de24df58f5d2a12c76b13d3d85ebc4125198
frikazoyd <frikazoyd@outlook.com>
2024-02-20 10:56:04 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,38 +12,46 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 计算接雨水的总量
+    /// 查找最长回文子串
     ///
-    /// 给定一个整数数组 `height`，其中每个元素代表一个条形块的高度，计算在完全下雨后，能够接多少雨水。
-    /// 实现思路基于动态编程，分别计算每个位置左侧和右侧的最大高度，然后根据每个位置的左右最大高度和当前高度差计算能接的雨水量。
+    /// 该方法通过扩展中心的方式来查找回文串，考虑了奇数长度和偶数长度的回文串。
     ///
-    /// - Parameter height: 一个整数数组，表示每个条形块的高度。
-    /// - Returns: 返回能接的雨水总量
-    func trap(_ height: [Int]) -> Int {
-        let n = height.count
-        if n <= 1 {
-            return 0
+    /// - Parameter s: 输入的字符串
+    /// - Returns: `s` 中最长的回文子串
+    func longestPalindrome(_ s: String) -> String {
+        let t = [Character](s)
+        var ans = ""
+        for i in 0..<t.count {
+            // 寻找以 t[i] 为中心的最长回文子串
+            let s1 = expandAroundCenter(t, i, i)
+            if s1.count > ans.count {
+                ans = s1
+            }
+            // 寻找以 t[i] 和 t[i+1] 为中心的最长回文子串
+            let s2 = expandAroundCenter(t, i, i + 1)
+            if s2.count > ans.count {
+                ans = s2
+            }
         }
+        return ans
+    }
 
-        // leftMax[i] 表示位置 i 左侧的最大高度，包括 i 本身
-        var leftMax = height
-        for i in 1..<n {
-            leftMax[i] = max(height[i], leftMax[i - 1])
+    /// 返回字符串 `s` 中以 `s[i]` 和 `s[j]` 为中心的最长回文子串
+    ///
+    /// 该函数尝试从中心向两边扩展，以找到最长的回文子串。它可以处理奇数长度和偶数长度的回文子串。
+    ///
+    /// - Parameters:
+    ///   - s: 字符数组，从原字符串转换得到。
+    ///   - i: 回文中心的左侧索引
+    ///   - j: 回文中心的右侧索引
+    /// - Returns: 以 `s[i]` 和 `s[j]` 为中心的最长回文子串
+    private func expandAroundCenter(_ s: [Character], _ i: Int, _ j: Int) -> String {
+        var i = i, j = j
+        while i >= 0 && j < s.count && s[i] == s[j] {
+            i -= 1
+            j += 1
         }
-
-        // rightMax[i] 表示位置 i 右侧的最大高度，包括 i 本身
-        var rightMax = height
-        for i in stride(from: n - 2, through: 0, by: -1) {
-            rightMax[i] = max(height[i], rightMax[i + 1])
-        }
-
-        // 计算能接的雨水总量
-        var sum = 0
-        for i in 0..<n {
-            // 对于每个位置，能接的雨水量等于它的左侧和右侧最大高度中较小的一个减去当前高度
-            sum += min(leftMax[i], rightMax[i]) - height[i]
-        }
-
-        return sum
+        // 注意：由于退出循环时，i 和 j 分别指向了回文子串外的第一个不匹配的字符，所以需要用 i+1 和 j-1 来截取
+        return String(s[i + 1..<j])
     }
 }
