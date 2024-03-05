67cc3ea57efae16e3afbc7666144c23f9316026e
frikazoyd <frikazoyd@outlook.com>
2024-03-05 11:57:26 +0800

    update

--- leetcode-swift/main.swift
+++ leetcode-swift/main.swift
@@ -12,31 +12,25 @@ public class TreeNode {
 }
 
 class Solution {
-    /// 计算并返回数组中最长连续序列的长度。
+    /// 将数组中所有的 0 移动到数组的末尾，同时保持非零元素的相对顺序。
     ///
-    /// 通过使用一个集合（Set）来处理数组，首先将所有元素添加到集合中去除重复项。然后遍历数组，对于每个元素，检查其前后是否有连续的数字存在，并同时从集合中移除这些连续的数字以避免重复计算。通过这种方式，可以找到以当前数字为起点的最长连续序列，并更新最长序列的长度。最终返回找到的最长序列长度。
-    ///
-    /// - Parameter nums: 整数数组，可能包含重复的元素。
-    /// - Returns: 数组中最长连续序列的长度。如果数组为空，则返回 0。
-    func longestConsecutive(_ nums: [Int]) -> Int {
-        var ans = 0
-        var numSet = Set(nums)
-        for x in nums {
-            if numSet.contains(x) {
-                var lo = x - 1
-                while numSet.contains(lo) {
-                    numSet.remove(lo)
-                    lo -= 1
-                }
-                var hi = x + 1
-                while numSet.contains(hi) {
-                    numSet.remove(hi)
-                    hi += 1
-                }
-                ans = max(ans, hi - lo - 1)
-                numSet.remove(x)
+    /// - Parameter nums: 一个引用传递的整型数组，可能包含 0 和非零整数。
+    func moveZeroes(_ nums: inout [Int]) {
+        // [0..i-1] != 0
+        // [i..j-1] == 0
+        // [j..n-1] Scanning
+        var i = 0
+        var j = 0
+        let n = nums.count
+        while j < n {
+            while j < n, nums[j] == 0 {
+                j += 1
+            }
+            if j < n {
+                nums.swapAt(i, j)
+                i += 1
+                j += 1
             }
         }
-        return ans
     }
 }
